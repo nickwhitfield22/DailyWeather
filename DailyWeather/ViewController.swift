46448b5622d6171672730a0c9f7354a4c71d3878
Nick <nicholaswhitfield@108-194-81-232.lightspeed.jcsnms.sbcglobal.net>
2018-10-18 01:09:38 -0500

    Initial Commit

---
+++ DailyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyWeather
+//
+//  Created by Nicholas Whitfield on 10/18/18.
+//  Copyright © 2018 RandomFacts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
